edc7d3219c4f6fecfe586f163cccf4007228d1a3
Peter Popa <118959968+PopaPeter@users.noreply.github.com>
2025-05-09 23:02:17 +0300

    Update tree.py

--- laboratories/cicd-documentation/tree.py
+++ laboratories/cicd-documentation/tree.py
@@ -62,24 +62,27 @@ class Tree:
             return self._find(data, node.right)
 
     def deleteTree(self):
+        # TODO 1
         self.root = None
 
     def printTree(self):
+        # TODO 1
         if self.root is not None:
             self._printInorderTree(self.root)
 
     def _printInorderTree(self, node):
+        # TODO 1
         if node is not None:
             self._printInorderTree(node.left)
             print(str(node.data) + ' ')
             self._printInorderTree(node.right)
 
     def _printPreorderTree(self, node):
-        # TODO
+        # TODO 2
         pass
 
     def _printPostorderTree(self, node):
-        # TODO
+        # TODO 2
         pass
 
 
